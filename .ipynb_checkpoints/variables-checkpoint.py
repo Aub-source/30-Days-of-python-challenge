55d0c0464d3534d2b6e0c595bae97a84a9a4f2cb
Aub <aubin.figa@gmail.com>
2025-07-26 23:50:10 +0000

    challenge Day4

---
+++ .ipynb_checkpoints/variables-checkpoint.py
@@ -0,0 +1,77 @@
+
+#Day 2: 30 Days of Python Programming
+
+prenoms="kodjo abel"
+
+nom_de famille="koffi"
+
+nom_complet="koffi kodjo abel"
+
+city="antannarivo"
+
+age=25
+
+year=2025
+
+is_married= "yes"
+
+is_true= "certainly"
+
+is_light= "vrai" 
+
+engin, marque, couleur, vendeur, annee= "voiture", "bmw", "noir", "portme", "2020"
+
+
+type(prenoms)
+
+type(nom_de famille)
+
+type(nom_complet)
+
+type(city)
+
+type(age)
+
+type(year)
+
+type(is_married)
+
+type(is_true)
+
+type(is_light)
+
+type(engin, marque, couleur, vendeur, annee)
+
+len(nom_complet)
+
+num_one= 5
+
+num_two= 4
+
+total= num_one + num_two
+
+diff= num_one - num_two
+
+product= num_one * num_two
+
+division= num_one/num_two
+
+remainder= num_one // num_two
+
+exp= num_one ** num_two
+
+
+raduis=30
+
+area_of_circle= 3.14 * raduis * raduis
+
+circum_of_circle= 3.14 * raduis * 2
+
+
+prenoms=input("Entrez votre prénom :")
+
+nom_de famille=input("Entrez votre nom de famille :")
+
+nom_complet=input("entrez votre nomplet :")
+
+pays=input("Entrez le nom d'un pays")
